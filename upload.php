7261f0746b02c50ab329cc5d51e66c7ba9069d2b
Omar <omar.torres11.upr.edu>
2024-02-15 22:33:32 -0400

    fourth commit

--- upload.php
+++ upload.php
@@ -1,15 +1,21 @@
 <?php
 if (isset($_POST['submit'])) {
-     $file = $_FILES['file']; 
-     $fileName = $file['name']; 
-     $fileTmpName = $file['tmp_name']; 
-     $fileSize = $file['size']; 
-     $fileError = $file['error'];
-    // Check if there is no error 
-if ($fileError === 0) { 
-    $uploadPath = 'uploads/' . $fileName; // Directory where to upload the file 
-    move_uploaded_file($fileTmpName, $uploadPath); 
-    echo "File uploaded successfully!";
-    } else { 
-        echo "Error uploading file."; 
-    } } ?>
+    $file = $_FILES['file'];
+
+    $fileName = $file['name'];
+    $fileTmpName = $file['tmp_name'];
+    $fileSize = $file['size'];
+    $fileError = $file['error'];
+
+    // Check if there is no error
+    if ($fileError === 0) {
+        $uploadPath = 'uploads/' . $fileName; // Directory where to upload the file
+        // echo $fileName;
+        move_uploaded_file($fileTmpName, $uploadPath);
+        echo "File uploaded successfully!";
+        // echo $_FILE['file'];
+    } else {
+        echo "Error uploading file.";
+    }
+}
+?>
